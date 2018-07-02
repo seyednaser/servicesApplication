d9057c7373c33fc98acabd486386bf510754f0b6
seyednaser <35557032+seyednaser@users.noreply.github.com>
2018-07-02 19:12:41 +0430

    Adding a title to MainActivity

---
+++ app/src/main/java/com/example/android/servicesapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.servicesapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onStartService(View view) {
+
+    }
+}
